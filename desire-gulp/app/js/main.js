837fc9ae9258a983933036010148734450fa9aa0
E-Martynenko1982 <motor8103@gmail.com>
2021-08-25 15:32:43 +0300

    адаптив

--- desire-gulp/app/js/main.js
+++ desire-gulp/app/js/main.js
@@ -1,23 +1,35 @@
 $(function () {
-  $('.header__btn').on('click', function () {
-    $('.rightside-menu').removeClass('rightside-menu--close')
-  })
+	$(".header__btn").on("click", function () {
+		$(".rightside-menu").removeClass("rightside-menu--close");
+	});
 });
 $(function () {
-  $('.rightside-menu__close').on('click', function () {
-    $('.rightside-menu').addClass('rightside-menu--close')
-  });
-  $('.top__slider').slick({
-    dots: true,
-    arrows: false,
-    fade: true,
-    autoplay: true
-  });
+	$(".rightside-menu__close").on("click", function () {
+		$(".rightside-menu").addClass("rightside-menu--close");
+	});
+	$(".top__slider").slick({
+		dots: true,
+		arrows: false,
+		fade: true,
+		autoplay: true,
+	});
 
-  var mixer = mixitup('.gallery__inner', {
-    load: {
-    filter: '.living'
-  }
+	$(".contact-slider").slick({
+		dots: true,
+		arrows: false,
+		slidesToShow: 10,
+		slidesToScroll: 10,
+	});
+
+	$(".article-slider__box").slick({
+    prevArrow: '<button type="button" class="article-slider__arrow article-slider__arrowright"> <img src="images/arrow-right.svg" alt="image"></button>',
+    nextArrow: '<button type="button" class="article-slider__arrow article-slider__arrowleft"> <img src="images/arrow-left.svg" alt="image"></button>',
+
+	});
+
+	var mixer = mixitup(".gallery__inner", {
+		load: {
+			filter: ".living",
+		},
+	});
 });
-
-})
